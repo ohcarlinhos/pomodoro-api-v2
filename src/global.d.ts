fd6b782ef5b2af9352a170a846d2988a951ca42f
Carlos Roberto <crpu4a@gmail.com>
2024-01-04 21:03:40 -0300

    chore: add new .env variables to processenv types

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,11 @@
-declare global {}
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            PG_HOST: string;
+            PG_PORT: number;
+            PG_DATABASE: string;
+            PG_USER: string;
+            PG_PASSWORD: string;
+        }
+    }
+}
