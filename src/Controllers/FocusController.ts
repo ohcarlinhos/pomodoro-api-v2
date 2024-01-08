a8aefb255c1105fb5dbdd1a6fcb473eb5322c48d
Carlos Roberto <crpu4a@gmail.com>
2024-01-08 20:24:29 -0300

    fix: type of FocusController

--- src/Controllers/FocusController.ts
+++ src/Controllers/FocusController.ts
@@ -1,7 +1,9 @@
+import { IFocus } from "../Domain/Focus/Types/IFocus";
 import { IController } from "./Types/IController";
 
 export class FocusController implements Partial<IController> {
-    GetAll<IFocus>(){
+    // TODO: implementar método
+    async GetAll() {
         const focus: IFocus[] = [];
         return focus;
     }
