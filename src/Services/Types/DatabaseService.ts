1dc279b93653c1c93c7a97dc66ac5c523670e49d
Carlos Roberto <crpu4a@gmail.com>
2024-01-08 07:25:11 -0300

    feat: create IService and DatabaseService

---
+++ src/Services/Types/DatabaseService.ts
@@ -0,0 +1,16 @@
+import { DatabaseClient } from "../../Database";
+import { Entity } from "../../Database/Entities/Types/Entity";
+import { IDatabaseClient } from "../../Database/Types/IDatabaseClient";
+import { IService } from "./IService";
+
+export abstract class DatabaseService implements IService {
+    protected _databaseClient: IDatabaseClient;
+    protected _entity: Entity;
+
+    constructor(entity: Entity) {
+        this._databaseClient = DatabaseClient;
+        this._entity = entity;
+    }
+
+    abstract Run(): Promise<void>;
+}
