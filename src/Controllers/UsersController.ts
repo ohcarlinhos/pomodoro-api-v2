b16fc84cba616afcbedc3b2891e73a752266b809
Carlos Roberto <crpu4a@gmail.com>
2024-01-07 23:04:56 -0300

    feat: add mock User routes

--- src/Controllers/UsersController.ts
+++ src/Controllers/UsersController.ts
@@ -3,7 +3,17 @@ import { User } from "../Domain/User/User";
 import { IController } from "./Types/IController";
 
 export class UsersController implements Partial<IController> {
-    Get(id: string): IUser {
-        return new User(id, "Carlos", "teste@test.com", "1234");
+    Get(id: string): Partial<IUser> {
+        const user = new User("Carlos", "teste@test.com", "1234");
+        user.Id = id;
+        return user.GetDTO();
+    }
+
+    GetAll(): Partial<IUser>[] {
+        return [
+            new User("Carlos", "teste@test.com", "1234").GetDTO(),
+            new User("Juninho", "juninho@test.com", "1234").GetDTO(),
+            new User("Aninha", "aninha@test.com", "1234").GetDTO(),
+        ];
     }
 }
--- src/Routes/Express/UsersRouter.ts
+++ src/Routes/Express/UsersRouter.ts
@@ -5,6 +5,10 @@ export class UsersRouter extends ExpressRouter {
     Main(): void {
         const usersController = new UsersController();
 
+        this._router.get("/", (_, res) => {
+            res.json(usersController.GetAll());
+        });
+
         this._router.get("/:id/", (req, res) => {
             if (req.params.id) res.json(usersController.Get(req.params.id));
         });
