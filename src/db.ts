a6b51dd3ab0d6cc06b826c9c722c732f9b786523
Carlos Roberto <crpu4a@gmail.com>
2024-01-08 07:23:04 -0300

    feat: change db

--- src/db.ts
+++ src/db.ts
@@ -1,24 +1,13 @@
 import "dotenv/config";
-import { DatabasePgClient } from "./Database/DatabasePgClient";
-import { DatabasePgConfig } from "./Database/DatabasePgConfig";
+
 import { MigrationHandler } from "./Database/Migrations/MigrationHandler";
-import { UserMigrationQueries } from "./Database/Migrations/Queries/UserMigrationQueries";
+import { DatabaseClient } from "./Database";
+import { MigrationQueries } from "./Database/Migrations";
 
-const migrationQueries = [new UserMigrationQueries()];
-
-async function doMigrates() {
-    const databaseConfig = new DatabasePgConfig();
-    const databaseClient = new DatabasePgClient(databaseConfig.Get());
-
-    await databaseClient.Connect();
-
-    const migrationHandle = new MigrationHandler(
-        databaseClient,
-        migrationQueries
-    );
-    await migrationHandle.Up();
-
-    await databaseClient.End();
+async function UpMigrates() {
+    await DatabaseClient.Connect();
+    await new MigrationHandler(DatabaseClient, MigrationQueries).Up();
+    await DatabaseClient.End();
 }
 
-doMigrates();
+UpMigrates();
