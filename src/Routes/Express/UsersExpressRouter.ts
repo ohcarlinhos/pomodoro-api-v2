f52f05d071a69794461290e537ea3b5625b4df86
Carlos Roberto <crpu4a@gmail.com>
2024-01-09 10:12:13 -0300

    feat: add users post route

--- src/Routes/Express/UsersExpressRouter.ts
+++ src/Routes/Express/UsersExpressRouter.ts
@@ -3,14 +3,20 @@ import { ExpressRouter } from "./Types/ExpressRouter";
 
 export class UsersExpressRouter extends ExpressRouter {
     Main(): void {
-        const usersController = new UsersController();
+        const controller = new UsersController();
 
         this._router.get("/", (_, res) => {
-            res.json(usersController.GetAll());
+            res.json(controller.GetAll());
         });
 
         this._router.get("/:id/", (req, res) => {
-            if (req.params.id) res.json(usersController.Get(req.params.id));
+            res.json(controller.Get(parseInt(req.params.id)));
+        });
+
+        this._router.post("/", async (req, res) => {
+            const { name, email, password } = req.body;
+            const result = await controller.Create({ name, email, password });
+            res.send(result);
         });
     }
 }
