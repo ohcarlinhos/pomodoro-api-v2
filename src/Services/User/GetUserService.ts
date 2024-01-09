dd19973bd1c1be55b16f06365eb0f01d79d477dc
Carlos Roberto <crpu4a@gmail.com>
2024-01-09 10:14:24 -0300

    feat: create mocked get user service

---
+++ src/Services/User/GetUserService.ts
@@ -0,0 +1,18 @@
+import { UserEntity } from "../../Database/Entities/UserEntity";
+import { DatabaseService } from "../Types/DatabaseService";
+
+export class GetUserService extends DatabaseService {
+    protected _entity: UserEntity;
+    private _id: number;
+
+    constructor(id: number) {
+        super();
+        this._entity = new UserEntity("users");
+        this._id = id;
+    }
+
+    async Run() {
+        // TODO: implementar serviço
+        // await this._entity.SelectById(this._id);
+    }
+}
