8de6a6577b49ddcfd18bd395380ebecbb6f31eab
Carlos Roberto <crpu4a@gmail.com>
2024-01-09 10:13:05 -0300

    feat: add Create to user controller with create user payload

--- src/Controllers/UsersController.ts
+++ src/Controllers/UsersController.ts
@@ -1,19 +1,33 @@
 import { IUser } from "../Domain/User/Types/IUser";
 import { User } from "../Domain/User/User";
+import { AddUserService } from "../Services/User/AddUserService";
 import { IController } from "./Types/IController";
 
+type CreateUserPayload = {
+    name: string;
+    email: string;
+    password: string;
+};
+
 export class UsersController implements Partial<IController> {
-    Get(id: string): Partial<IUser> {
-        const user = new User("Carlos", "teste@test.com", "1234");
+    async Get(id: number): Promise<Partial<IUser>> {
+        const user = await new User("Carlos", "teste@test.com");
         user.Id = id;
         return user.GetDTO();
     }
 
-    GetAll(): Partial<IUser>[] {
+    async GetAll(): Promise<Partial<IUser>[]> {
         return [
-            new User("Carlos", "teste@test.com", "1234").GetDTO(),
-            new User("Juninho", "juninho@test.com", "1234").GetDTO(),
-            new User("Aninha", "aninha@test.com", "1234").GetDTO(),
+            new User("Carlos", "teste@test.com").GetDTO(),
+            new User("Juninho", "juninho@test.com").GetDTO(),
+            new User("Aninha", "aninha@test.com").GetDTO(),
         ];
+    }
+
+    async Create(user: CreateUserPayload): Promise<Partial<IUser>> {
+        const { name, email, password } = user;
+        const newUser = new User(name, email);
+        newUser.Password = password;
+        return await new AddUserService(newUser).Run();
     }
 }
