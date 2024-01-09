bdadb6c45b25f7a2c9ff1a533b3752b023382bb2
Carlos Roberto <crpu4a@gmail.com>
2024-01-09 10:13:23 -0300

    fix: focus express router

--- src/Routes/Express/FocusExpressRouter.ts
+++ src/Routes/Express/FocusExpressRouter.ts
@@ -1,12 +1,12 @@
-import { UsersController } from "../../Controllers/UsersController";
+import { FocusController } from "../../Controllers/FocusController";
 import { ExpressRouter } from "./Types/ExpressRouter";
 
 export class FocusExpressRouter extends ExpressRouter {
     Main(): void {
-        const usersController = new UsersController();
+        const controller = new FocusController();
 
         this._router.get("/", (req, res) => {
-            res.json([]);  
+            res.json([]);
         });
     }
 }
