87fa849372c1baf1a87a1064fa491eb92952c95e
Carlos Roberto <crpu4a@gmail.com>
2024-01-09 22:34:20 -0300

    feat: add ExpressErrorHandlerConfig to server config

--- src/init.ts
+++ src/init.ts
@@ -1,10 +1,14 @@
 import "dotenv/config";
+import "express-async-errors";
+
 import { ExpressServer } from "./Server/ExpressServer";
 import { ExpressCorsConfig } from "./Server/ExpressCorsConfig";
 import { ExpressRoutesConfig } from "./Server/ExpressRoutesConfig";
 import { ExpressRouterV1Handler } from "./Routes";
+import { ExpressErrorHandlerConfig } from "./Server/ExpressErrorHandlerConfig";
 
 const App = new ExpressServer(3000);
 App.Add(new ExpressCorsConfig());
 App.Add(new ExpressRoutesConfig(ExpressRouterV1Handler.Router()));
+App.Add(new ExpressErrorHandlerConfig());
 App.Run();
