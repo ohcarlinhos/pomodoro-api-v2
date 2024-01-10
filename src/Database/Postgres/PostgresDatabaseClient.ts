c7291df198f4f73a826ffc961beb1eeba98e60ed
Carlos Roberto <crpu4a@gmail.com>
2024-01-09 22:33:53 -0300

    feat: create OnlyQuery and change Query method

--- src/Database/Postgres/PostgresDatabaseClient.ts
+++ src/Database/Postgres/PostgresDatabaseClient.ts
@@ -2,6 +2,7 @@ import { Client } from "pg";
 
 import { IDatabaseClient } from "../Types/IDatabaseClient";
 import { PostgresDatabaseConfig } from "./PostgresDatabaseConfig";
+import { CustomDatabaseError } from "../../Shared/Errors/CustomDatabaseError";
 
 export class PostgresDatabaseClient implements IDatabaseClient {
     private _client: Client;
@@ -18,7 +19,19 @@ export class PostgresDatabaseClient implements IDatabaseClient {
         await this._client.end();
     }
 
-    async Query(queryString: string) {
+    async OnlyQuery(queryString: string) {
         return this._client.query(queryString);
+    }
+
+    async Query(queryString: string) {
+        try {
+            await this.Connect();
+            const result = await this._client.query(queryString);
+            return result;
+        } catch (err) {
+            throw new CustomDatabaseError((err as Error).message);
+        } finally {
+            await this.End();
+        }
     }
 }
