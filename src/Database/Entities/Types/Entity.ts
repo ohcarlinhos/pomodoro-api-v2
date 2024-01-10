c9dedd63446590ee728a0e55f497e449d85c2782
Carlos Roberto <crpu4a@gmail.com>
2024-01-09 22:56:27 -0300

    feat: change Entity default methods e database client object

--- src/Database/Entities/Types/Entity.ts
+++ src/Database/Entities/Types/Entity.ts
@@ -1,46 +1,40 @@
 import { PostgresDatabaseClient } from "../../Postgres/PostgresDatabaseClient";
-import { DatabaseClient } from "../..";
+import { PostgresDatabaseConfig } from "../../Postgres/PostgresDatabaseConfig";
 
 export abstract class Entity {
-    protected _databaseClient: PostgresDatabaseClient;
     protected _tableName: string;
+    protected _databaseClient: PostgresDatabaseClient;
 
     constructor(tableName: string) {
-        this._databaseClient = DatabaseClient;
         this._tableName = tableName;
+        this._databaseClient = new PostgresDatabaseClient(
+            new PostgresDatabaseConfig()
+        );
     }
 
     protected async _selectById(id: number, cols = ["*"]) {
-        try {
-            await this._databaseClient.Connect();
-            const result = await this._databaseClient.Query(`
-                select
-                        ${cols.join(", ")}
-                from
-                        ${this._tableName}
-                where
-                        id = ${id};
-            `);
-            return result.rows;
-        } finally {
-            await this._databaseClient.End();
-        }
+        const result = await this._databaseClient.Query(`
+            select
+                    ${cols.join(", ")}
+            from
+                    ${this._tableName}
+            where
+                    id = ${id};
+        `);
+
+        return result.rows;
     }
 
     protected async _selectByUuid(uuid: string, cols = ["*"]) {
-        try {
-            await this._databaseClient.Connect();
-            const result = await this._databaseClient.Query(`
-                select
-                        ${cols.join(", ")}
-                from
-                        ${this._tableName}
-                where
-                        id = ${uuid};
-            `);
-            return result.rows;
-        } finally {
-            await this._databaseClient.End();
-        }
+        const result = await this._databaseClient.Query(`
+            select
+                    ${cols.join(", ")}
+            from
+                    ${this._tableName}
+            where
+                    id = ${uuid};
+        `);
+
+        return result.rows;
     }
 }
