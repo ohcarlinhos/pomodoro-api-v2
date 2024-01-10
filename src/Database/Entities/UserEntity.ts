ea35efafca28465822c086a1a32df8050ada56df
Carlos Roberto <crpu4a@gmail.com>
2024-01-09 22:57:18 -0300

    feat: change UserEntity methods and implement SelectById and _mountUser

--- src/Database/Entities/UserEntity.ts
+++ src/Database/Entities/UserEntity.ts
@@ -1,6 +1,7 @@
 import { IUser } from "../../Domain/User/Types/IUser";
 import { Entity } from "./Types/Entity";
 import { User } from "../../Domain/User/User";
+import { CustomDatabaseErrorByTag } from "../../Shared/Errors/CustomDatabaseErrorByTag";
 
 interface UserDB {
     id: number;
@@ -11,26 +12,30 @@ interface UserDB {
 
 export class UserEntity extends Entity {
     async Create(user: IUser): Promise<IUser> {
-        try {
-            await this._databaseClient.Connect();
+        const result = await this._databaseClient.Query(`
+            insert into ${this._tableName} (name, email, password)
+            values (
+                '${user.Name}',
+                '${user.Email}',
+                '${user.Password}'
+            )
+            returning *;
+        `);
 
-            const result = await this._databaseClient.Query(`
-                insert into ${this._tableName} (name, email, password)
-                values (
-                    '${user.Name}',
-                    '${user.Email}',
-                    '${user.Password}'
-                )
-                returning *;
-            `);
+        return this._mountUser(result.rows[0]);
+    }
 
-            const userDB = result.rows[0] as UserDB;
-            const newUser = new User(userDB.name, userDB.email);
-            newUser.Id = userDB.id;
+    async SelectById(id: number): Promise<IUser> {
+        const [result] = await this._selectById(id);
+        return this._mountUser(result);
+    }
 
-            return newUser;
-        } finally {
-            await this._databaseClient.End();
-        }
+    private _mountUser(item: unknown): IUser {
+        if (!item) throw new CustomDatabaseErrorByTag("user_not_found");
+
+        const userDB = item as UserDB;
+        const newUser = new User(userDB.name, userDB.email);
+        newUser.Id = userDB.id;
+        return newUser;
     }
 }
